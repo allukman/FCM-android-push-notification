31ac49885f9e1e29d587f1929fa263bb49d865ea
Al Lukman <allukman1802@gmail.com>
2022-12-14 11:32:06 +0700

    init firebase

---
+++ app/src/main/java/com/karsatech/androidpushnotification/MyFirebaseMessagingService.kt
@@ -0,0 +1,7 @@
+package com.karsatech.androidpushnotification
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+}
